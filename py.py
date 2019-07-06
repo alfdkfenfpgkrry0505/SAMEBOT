1648a88ecf7e306ec57a56e8faee4c0c0c71b5a1
alfdkfenfpgkrry0505 <51285169+alfdkfenfpgkrry0505@users.noreply.github.com>
2019-07-06 15:46:36 +0900

    Update py.py

--- py.py
+++ py.py
@@ -1,1 +1,33 @@
+#Work with Python 3.7.3
+import asyncio, discord, datetime, logging, random, traceback, time, os
+from discord.ext import commands
 
+app = discord.Client()
+
+
+@app.event
+async def on_ready():
+    print('로그인 중 입니다 ..!')
+    print(app.user.name)
+    print(app.user.id)
+    print('===============')
+    game = discord.Game("쌔임에게 ?도와줘라고 도움을 요청하거라!")
+    await app.change_presence(status=discord.Status.online, activity=game)
+    
+
+        
+@app.event
+async def on_message(message):
+
+    if message.content.startswith("?도와줘"):
+        embed = discord.Embed(title="  ", description=" 안녕? 난 쌔임이야. 아래는 명령어 목록이야.", color=0xff0000)
+        await message.channel.send(embed=embed)
+        embed = discord.Embed(title=" 명령어 목록 ", description=" ", color=0xff0000)
+        await message.channel.send(embed=embed)
+        embed = discord.Embed(title=" ?안녕 ", description=" 쌔임이 인사를 해줍니다 ", color=0xff0000)
+
+
+
+
+accross_token = os.environ["BOT_TOKEN"]
+app.run(accross_token)
