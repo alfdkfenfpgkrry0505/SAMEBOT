00db352247ad0b81ca009c9f7770460b510e9c02
alfdkfenfpgkrry0505 <51285169+alfdkfenfpgkrry0505@users.noreply.github.com>
2019-07-06 15:57:59 +0900

    Update py.py

--- py.py
+++ py.py
@@ -11,7 +11,7 @@ async def on_ready():
     print(app.user.name)
     print(app.user.id)
     print('===============')
-    game = discord.Game("쌔임에게 ?도와줘라고 도움을 요청하거라!")
+    game = discord.Game("쌔임에게 ?도와줘라고 도움을 요청해봐!")
     await app.change_presence(status=discord.Status.online, activity=game)
     
 
@@ -25,6 +25,36 @@ async def on_message(message):
         embed = discord.Embed(title=" 명령어 목록 ", description=" ", color=0xff0000)
         await message.channel.send(embed=embed)
         embed = discord.Embed(title=" ?안녕 ", description=" 쌔임이 인사를 해줍니다 ", color=0xff0000)
+        await message.channel.send(embed=embed)
+        embed = discord.Embed(title=" ?자기소개 ", description=" 쌔임이 자기소개를 해줍니다 ", color=0xff0000)
+        await message.channel.send(embed=embed)
+        embed = discord.Embed(title=" ?패치노트 ", description=" 쌔임의 패치노트를 불러옵니다 ", color=0xff0000)
+        await message.channel.send(embed=embed)
+        
+    if message.content.startswith("?자기소개"):
+        embed = discord.Embed(title="안녕? 난 쌔임이야.  ", description="", color=0xffaaaa)
+        await message.channel.send(embed=embed)
+        embed = discord.Embed(title="내 이름은   ", description="", color=0xffaaaa)
+        await message.channel.send(embed=embed)
+        embed = discord.Embed(title="[Special gift are made by effort.]  ", description="", color=0xffaaaa)
+        await message.channel.send(embed=embed)
+        embed = discord.Embed(title="[특별한 재능은 노력으로 만들어진다.] 라는 의미를 가진 약자야.   ", description="", color=0xffaaaa)
+        await message.channel.send(embed=embed)
+        embed = discord.Embed(title="뭐 도움받고 싶은 것이 있다면 ?도와줘를 대화창에다 입력해봐.   ", description="", color=0xffaaaa)
+        await message.channel.send(embed=embed)
+        
+     if message.content.startswith("?안녕"):
+        msg = "{0.author.mention} 안녕? 반갑다.".format(message)
+        await message.channel.send( msg)
+        
+     if message.content.startswith("?패치노트"):
+        embed = discord.Embed(title="2019.07.06.토요일  ", description="1.0.0 개발 시작!", color=0xffaaaa)
+        await message.channel.send(embed=embed)
+        
+        
+
+        
+
 
 
 
